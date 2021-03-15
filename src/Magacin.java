5d8a6b2b26b8e36b921d00fc7d67e4f2040d4a0d
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-15 10:24:43 +0100

    Uvedena klasa Magacin koja nasledjuje MagacinInterfejs sa metodama

---
+++ src/Magacin.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+
+import interfejs.MagacinInterfejs;
+import magacin.Artikli;
+
+public class Magacin implements MagacinInterfejs {
+	
+	LinkedList<Artikli> artikli = new LinkedList<Artikli>();
+
+	@Override
+	public void dodavanjeArtikla(Artikli a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisanjeArtikla(int kolicina) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String nadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
