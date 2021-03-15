5daeaefa53e92577eda41cb693e7b30dc04d47aa
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-15 10:47:40 +0100

    Implementirao sam metode i dodao parametar u nadjiArtikal pa morama da mrdzujem da master granom

--- src/Magacin.java
+++ src/Magacin.java
@@ -1,5 +1,5 @@
 import java.util.LinkedList;
-
+import java.util.*;
 import interfejs.MagacinInterfejs;
 import magacin.Artikli;
 
@@ -7,23 +7,30 @@ public class Magacin implements MagacinInterfejs {
 	
 	LinkedList<Artikli> artikli = new LinkedList<Artikli>();
 
-	@Override
-	public void dodavanjeArtikla(Artikli a) {
 	
+	public void dodavanjeArtikla(Artikli a) {
+	for (Artikli ar: artikli)
+		if (!artikli.contains(a))
+			artikli.add(a);
 		
 	}
 
 	@Override
 	public void brisanjeArtikla(Artikli a, int kolicina) {
+		for (Artikli ar:artikli)
+		if (artikli.contains(a))
+			a.setKolicina(a.getKolicina()-kolicina);
+
+
+	}
+
+	
+	public String nadjiArtikal( int sifra) {
+		for (int i=0; i<artikli.size(); i++) 
+			if (artikli.get(i).getSifra()==sifra) 
+			
 		
-
-
+		return artikli.get(i).getNaziv();
 	}
-
-	@Override
-	public String nadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
--- src/interfejs/MagacinInterfejs.java
+++ src/interfejs/MagacinInterfejs.java
@@ -7,7 +7,7 @@ public interface MagacinInterfejs {
 	
 	public void dodavanjeArtikla (Artikli a);
 	
-	public void brisanjeArtikla (int kolicina);
+	public void brisanjeArtikla (Artikli a, int kolicina);
 	
-	public String nadjiArtikal (Artikli a,int sifra);
+	public String nadjiArtikal (int sifra);
 }
