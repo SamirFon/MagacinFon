03bd8548c96bf85f3674fee4f2fb05ece67cc699
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-15 11:22:02 +0100

    Ubacen parametar Artikli a

--- src/interfejs/MagacinInterfejs.java
+++ src/interfejs/MagacinInterfejs.java
@@ -7,7 +7,7 @@ public interface MagacinInterfejs {
 	
 	public void dodavanjeArtikla (Artikli a);
 	
-	public void brisanjeArtikla (int kolicina);
+	public void brisanjeArtikla (Artikli a,int kolicina);
 	
 	public String nadjiArtikal (int sifra);
 }
