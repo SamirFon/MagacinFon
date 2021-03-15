dee7d664bfe14b30cdb0db975472f268b09c3e38
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-15 09:10:30 +0100

    Uradjene metode toString, equals i hashCode

--- src/magacin/KancelarijskiMaterijal.java
+++ src/magacin/KancelarijskiMaterijal.java
@@ -32,6 +32,46 @@ public class KancelarijskiMaterijal extends Artikli {
 	public void setKolicina(int kolicina) {
 		this.kolicina = kolicina;
 	}
+	@Override
+	public String toString() {
+		return "KancelarijskiMaterijal [naziv=" + naziv + ", opis=" + opis + ", sifra=" + sifra + ", kolicina="
+				+ kolicina + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + kolicina;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((opis == null) ? 0 : opis.hashCode());
+		result = prime * result + sifra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof KancelarijskiMaterijal))
+			return false;
+		KancelarijskiMaterijal other = (KancelarijskiMaterijal) obj;
+		if (kolicina != other.kolicina)
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (opis == null) {
+			if (other.opis != null)
+				return false;
+		} else if (!opis.equals(other.opis))
+			return false;
+		if (sifra != other.sifra)
+			return false;
+		return true;
+	}
 	
 	
 	
--- src/magacin/Knjige.java
+++ src/magacin/Knjige.java
@@ -32,6 +32,45 @@ public class Knjige extends Artikli {
 	public void setKolicina(int kolicina) {
 		this.kolicina = kolicina;
 	}
+	@Override
+	public String toString() {
+		return "Knjige [naziv=" + naziv + ", opis=" + opis + ", sifra=" + sifra + ", kolicina=" + kolicina + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + kolicina;
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((opis == null) ? 0 : opis.hashCode());
+		result = prime * result + sifra;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (!(obj instanceof Knjige))
+			return false;
+		Knjige other = (Knjige) obj;
+		if (kolicina != other.kolicina)
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (opis == null) {
+			if (other.opis != null)
+				return false;
+		} else if (!opis.equals(other.opis))
+			return false;
+		if (sifra != other.sifra)
+			return false;
+		return true;
+	}
 	
 	
 
