2903882768c08e2b3e0b301f0c0dba079ee4290d
SamirFon <samir.mehmedovic@fon.bg.ac.rs>
2021-03-15 10:10:45 +0100

    Dodat paket intefejs, kao i interfejs MagacinInterfejs sa metodama

---
+++ src/interfejs/MagacinInterfejs.java
@@ -0,0 +1,13 @@
+package interfejs;
+
+import magacin.Artikli;
+
+public interface MagacinInterfejs {
+
+	
+	public void dodavanjeArtikla (Artikli a);
+	
+	public void brisanjeArtikla (int kolicina);
+	
+	public String nadjiArtikal (int sifra);
+}
